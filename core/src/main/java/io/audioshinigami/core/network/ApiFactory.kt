714de1c696293d92d3bc5040b95c0562dd5df9c7
david <hirakoshinji722@gmail.com>
2020-09-14 13:09:19 +0100

    add ApiFactory.kt object to provide RickAndMortyService retrofit instance

--- core/src/main/java/io/audioshinigami/core/network/ApiFactory.kt
+++ core/src/main/java/io/audioshinigami/core/network/ApiFactory.kt
@@ -1,4 +1,49 @@
 package io.audioshinigami.core.network
 
+import io.audioshinigami.core.BuildConfig
+import io.audioshinigami.core.network.services.RickAndMortyService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiFactory {
+
+    private val API_BASE_URL = "https://rickandmortyapi.com/api"
+
+    /**
+     *  Provider method for [HttpLoggingInterceptor] for HTTP client
+     *
+     *  @return Instance of http interceptor
+     */
+    private fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+         HttpLoggingInterceptor().apply { level =  HttpLoggingInterceptor.Level.BODY}
+
+    /**
+     *  Provides instance of [OkHttpClient]
+     *
+     *  @return Instance of http client
+     */
+    private fun provideHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
+        val clientBuilder = OkHttpClient.Builder()
+        if( BuildConfig.DEBUG){
+            clientBuilder.addInterceptor( interceptor)
+        }
+        return clientBuilder.build()
+    }
+
+    /**
+     * Provider method for [Retrofit]
+     *
+     * @return Instance of retrofit
+     */
+    private fun provideRetrofitBuilder() =
+        Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client( provideHttpClient( provideHttpLoggingInterceptor() ))
+            .build()
+
+    fun provideRickAndMortyService() =
+        provideRetrofitBuilder().create(RickAndMortyService::class.java)
 }
