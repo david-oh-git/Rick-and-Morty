a0cffaa0a7a318f034742942189f306efc55abc2
david <hirakoshinji722@gmail.com>
2020-10-02 11:46:08 +0100

    add base mapper interface

---
+++ core/src/main/java/io/audioshinigami/core/mapper/Mapper.kt
@@ -0,0 +1,13 @@
+package io.audioshinigami.core.mapper
+
+/**
+ *A base helper to transform an input to a desired output.
+ */
+interface Mapper<F,T> {
+
+    /**
+     * @param from Object to be transformed.
+     * @return The desired output.
+     */
+    suspend fun transform(from: F): T
+}
